5fd9230acaa7ee95569d1215ca709f7785f681f7
thiagofr99 <linkinht@gmail.com>
2022-05-30 18:52:11 -0300

    core: correção de config

--- FullStackChallenge/src/main/java/com/devthiagofurtado/fullstackchallenge/config/RedisDemoConfiguration.java
+++ FullStackChallenge/src/main/java/com/devthiagofurtado/fullstackchallenge/config/RedisDemoConfiguration.java
@@ -4,13 +4,9 @@ import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCust
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 
-import java.awt.print.Book;
 import java.time.Duration;
 
 @Configuration
@@ -32,16 +28,5 @@ public class RedisDemoConfiguration {
                 .withCacheConfiguration("customerCache",
                         RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(5)));
     }
-
-    @Bean
-    JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
-    }
-
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(jedisConnectionFactory());
-        return template;
-    }
+    
 }
